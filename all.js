a37116a99a85a968f4ebf8b48ebd1f7c4c71e913
Colleen Kingsley <colleen.kingsley89@gmail.com>
2020-07-02 19:59:47 -0400

    Complete xor.js and all.js

--- all.js
+++ all.js
@@ -1,6 +1,17 @@
 function all(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == false) {
+			return false
+		}
+		return true
+	}
+}
+	let array1 = [true, false, false]
+	let array2 = [true, true, true]
+	console.log(all(array1))
+	console.log(all(array2))
+	
 	// Loop through every item in the array
 	// If the item evaluates to false, return false
 
 	// If the loop ends, return true
-}
