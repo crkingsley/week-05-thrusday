7000ffe3fc1f90c489f0d2fc0f96ffc2335bf996
Colleen Kingsley <colleen.kingsley89@gmail.com>
2020-07-05 16:55:49 -0400

    Create functions for dropRight and flatten

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,16 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
+function dropRight(arr, numberOfItemsToDrop) {
+	// if n is larger than array length return []
+	if (numberOfItemsToDrop >= arr.length) {
+		return arr = []
+	}
+	// Use n to count in from the end to get the index
+	let itemsToBeDropped = (arr.length - numberOfItemsToDrop)
 
-	// If the loop ends, return true
+	// return a slice of the array using the index as the end
+	return arr.slice(0, itemsToBeDropped)
 }
+
+
+n = 2
+sampleArr = [2, 3, 6, 7, 9]
+console.log(dropRight(sampleArr, n))
--- flatten.js
+++ flatten.js
@@ -1,9 +1,20 @@
-function flatten(arr) {
+function flatten(arrItems) {
 	// Create a new array
-
+	let newArray = []
 	// Loop through every item in the array
+	arrItems.forEach((arrItem) => {
+		if (Array.isArray) {
+			newArray = newArray.concat(arrItem)
+		}else {
+			newArray.push(arrItem)
+		}
+	})
+	return newArray
+
 	// If the item is an array, add its items to the new array
 	// If not add the item itself to the new array
 
 	// return the new array
 }
+sampleArray = [3, [3, 5], 7, 9]
+console.log(flatten(sampleArray))
